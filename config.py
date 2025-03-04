c62ac866279df9bd98c8fb153b90d7400c4b2705
Darkdevolt <diopibrahimabdallah@gmail.com>
2025-03-05 00:59:25 +0100

    config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+PARAMETRES = {
+    "DEVISE": "XOF",
+    "MONTANT_MIN": 10000,
+    "COLONNES": [
+        "Exchange Date", "Close", "Net", "%Chg", "Open", 
+        "Low", "High", "Volume", "Turnover - XOF", "Flow"
+    ]
+}
