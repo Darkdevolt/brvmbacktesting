d8da93417cd404bc903c68da82dbdc5c9c759da3
Darkdevolt <diopibrahimabdallah@gmail.com>
2025-03-07 02:54:48 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-PARAMETRES = {
-    "DEVISE": "XOF",
-    "MONTANT_MIN": 10000,
-    "COLONNES": [
-        "Exchange Date", "Close", "Net", "%Chg", "Open", 
-        "Low", "High", "Volume", "Turnover - XOF", "Flow"
-    ]
-}
